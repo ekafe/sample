62de3393a30dd01cf0f768b1586d229bb12d277b
muhammednabil.km <muhammednabil.km@utthunga.com>
2023-02-03 10:57:05 +0530

    necessary files

---
+++ program1.py
@@ -0,0 +1,22 @@
+#############################################
+# FILENAME : program1.py
+# AUTHOR_CODE :
+# Date :
+# DESCRIPTION :
+# COPYRIGHT :
+#############################################
+
+'''
+Read the Input File - input.json
+Its an array entity with name, value and values
+Find the average of the values of each one and compare if its below or above average
+
+Print Out in this Format
+(Sl.No - entityPropertyId - value - AverageofValues - below/above average/NA)
+1 - Methane(CH4) - 40 - 45 - Below Average
+2 - Property1(CH4) - 40 - 45 - Above Average
+3 - Property12 - 1 - NA - NA 
+'''
+
+if __name__ == '__main__':
+    print("hello world")
---
+++ program2.py
@@ -0,0 +1,18 @@
+#############################################
+# FILENAME : program1.py
+# AUTHOR_CODE :
+# Date :
+# DESCRIPTION :
+# COPYRIGHT :
+#############################################
+
+'''
+Read the Image Input - image.jpg
+1 - Find the Dial Circle - Find the Circle and draw border
+2 - Crop the Circle 
+3 - Rotate Cropped Circle to turn clockwise 30degree
+4 - Save it into image file
+'''
+
+if __name__ == '__main__':
+    print("hello world")
